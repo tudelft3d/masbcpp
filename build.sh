6453a0ea206bd4aa57dc356c893c225a3580cf89
Ylannl <r.y.peters@tudelft.nl>
2015-04-30 11:05:01 +0200

    use homebrew vrui

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-g++ -o compute_ma src/compute_ma.cpp -lcnpy -I/Users/ravi/Vrui-3.1/include  -L/Users/ravi/Vrui-3.1/lib  -lGeometry.g++-3 -lIO.g++-3 -lMath.g++-3 -lMisc.g++-3 -lThreads.g++-3  -lz
+g++ -o compute_ma src/compute_ma.cpp -lcnpy -I/usr/local/include/vrui  -L/usr/local/lib/vrui  -lGeometry.g++-3 -lIO.g++-3 -lMath.g++-3 -lMisc.g++-3 -lThreads.g++-3  -lz
