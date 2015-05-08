ea7ece8376f20179971812ca2622742d4bc3ad4e
Ravi Peters <r.y.peters@tudelft.nl>
2015-05-08 23:05:57 +0200

    remove outdated scripts

--- build_openmp.sh
+++
@@ -1,1 +0,0 @@
-clang-omp++ -o compute_ma src/*.cpp thirdparty/vrui/Geometry/*.cpp thirdparty/vrui/Misc/*.cpp thirdparty/cnpy/*.cpp thirdparty/kdtree2/*.cpp -O3 -funroll-loops -ffast-math -Ithirdparty/vrui -liomp5 -fopenmp
