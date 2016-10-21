0fac7a665743bcdcc25587b3d5f2df98ad984595
Jeff Coukell <jeff.coukell@safe.com>
2016-10-21 13:39:15 -0700

    Switch compute_ma to Eigen and pcl

--- src/compute_ma.cpp
+++ src/compute_ma.cpp
@@ -27,6 +27,8 @@ SOFTWARE.
 #include <tclap/CmdLine.h>
 
 #include "compute_ma_processing.h"
+#include "io.h"
+#include "madata.h"
 #include "types.h"
 
 int main(int argc, char **argv) {
@@ -42,7 +44,6 @@ int main(int argc, char **argv) {
       TCLAP::ValueArg<double> initial_radiusArg("r", "radius", "initial ball radius", false, 200, "double", cmd);
 
       TCLAP::SwitchArg nan_for_initrSwitch("a", "nan", "write nan for points with radius equal to initial radius", cmd, false);
-      TCLAP::SwitchArg reorder_kdtreeSwitch("N", "no-kdtree-reorder", "Don't reorder kd-tree points: slower computation but lower memory use", cmd, true);
 
       cmd.parse(argc, argv);
 
@@ -51,9 +52,7 @@ int main(int argc, char **argv) {
       input_parameters.initial_radius = float(initial_radiusArg.getValue());
       input_parameters.denoise_preserve = (M_PI / 180.0) * denoise_preserveArg.getValue();
       input_parameters.denoise_planar = (M_PI / 180.0) * denoise_planarArg.getValue();
-
       input_parameters.nan_for_initr = nan_for_initrSwitch.getValue();
-      input_parameters.kd_tree_reorder = reorder_kdtreeSwitch.getValue();
 
       std::string output_path = inputArg.getValue();
       if (outputArg.isSet())
@@ -91,70 +90,27 @@ int main(int argc, char **argv) {
 
       std::cout << "Parameters: denoise_preserve=" << denoise_preserveArg.getValue() << ", denoise_planar=" << denoise_planarArg.getValue() << ", initial_radius=" << input_parameters.initial_radius << "\n";
 
+      io_parameters io_params = {};
+      io_params.coords = true;
+      io_params.normals = true;
       ma_data madata = {};
 
-      cnpy::NpyArray coords_npy = cnpy::npy_load(input_coords_path.c_str());
-      float* coords_carray = reinterpret_cast<float*>(coords_npy.data);
+      madata.coords = PointCloud::Ptr(new PointCloud);
+      madata.normals = NormalCloud::Ptr(new NormalCloud);
+      madata.ma_coords = PointCloud::Ptr(new PointCloud);
 
-      madata.m = coords_npy.shape[0];
-      unsigned int dim = coords_npy.shape[1];
-      PointList coords(madata.m);
-      for (unsigned int i = 0; i < madata.m; i++) coords[i] = Point(&coords_carray[i * 3]);
-      coords_npy.destruct();
+      npy2madata(inputArg.getValue(), madata, io_params);
 
-      cnpy::NpyArray normals_npy = cnpy::npy_load(input_normals_path.c_str());
-      float* normals_carray = reinterpret_cast<float*>(normals_npy.data);
-      VectorList normals(normals_npy.shape[0]);
-      for (unsigned int i = 0; i < madata.m; i++) normals[i] = Vector(&normals_carray[i * 3]);
-      normals_npy.destruct();
-
-      // Storage space for our results:
-      PointList ma_coords(2 * madata.m);
-
-      madata.coords = &coords;
-      madata.normals = &normals;
-      madata.ma_coords = &ma_coords;
-      madata.ma_qidx = new int[2 * madata.m];
+      madata.ma_qidx.resize(2 * madata.m);
 
       // Perform the actual processing
       compute_masb_points(input_parameters, madata);
 
-      // Write out the results for the inside
-      Scalar* ma_coords_in_carray = new Scalar[madata.m * 3];
-      for (int i = 0; i < madata.m; i++)
-         for (int j = 0; j < 3; j++)
-            ma_coords_in_carray[i * 3 + j] = ma_coords[i][j];
-
-      const unsigned int c_size_in = madata.m;
-      const unsigned int shape_in[] = { c_size_in,3 };
-      cnpy::npy_save(output_path_ma_in.c_str(), ma_coords_in_carray, shape_in, 2, "w");
-      const unsigned int shape_in_[] = { c_size_in };
-      cnpy::npy_save(output_path_ma_q_in.c_str(), madata.ma_qidx, shape_in_, 1, "w");
-
-      // Write out the results for the outside
-      Scalar* ma_coords_out_carray = new Scalar[madata.m * 3];
-      for (int i = 0; i < madata.m; i++)
-         for (int j = 0; j < 3; j++)
-            ma_coords_out_carray[i * 3 + j] = ma_coords[i + madata.m][j];
-
-      const unsigned int c_size_out = madata.m;
-      const unsigned int shape_out[] = { c_size_out,3 };
-      cnpy::npy_save(output_path_ma_out.c_str(), ma_coords_out_carray, shape_in, 2, "w");
-      const unsigned int shape_out_[] = { c_size_out };
-      cnpy::npy_save(output_path_ma_q_out.c_str(), &madata.ma_qidx[madata.m], shape_out_, 1, "w");
-
-      {
-         std::ofstream metadata(output_path_metadata.c_str());
-         metadata
-            << "initial_radius " << input_parameters.initial_radius << std::endl
-            << "nan_for_initr " << input_parameters.nan_for_initr << std::endl
-            << "denoise_preserve " << denoise_preserveArg.getValue() << std::endl
-            << "denoise_planar " << denoise_planarArg.getValue() << std::endl;
-         metadata.close();
-      }
-      // Free up memory
-      delete[] madata.ma_qidx; madata.ma_qidx = NULL;
-
+      io_params.coords = false;
+      io_params.normals = false;
+      io_params.ma_coords = true;
+      io_params.ma_qidx = true;
+      madata2npy(output_path, madata, io_params);
    }
    catch (TCLAP::ArgException &e) { std::cerr << "Error: " << e.error() << " for " << e.argId() << std::endl; }
 
